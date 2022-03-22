5260d28fdb8de4c5b7a4e2c3f5bef3ee59252f35
Vishal Rathod <rvd31305@gmail.com>
2022-03-22 15:41:40 +0530

    Added Tab List

---
+++ src/components/TabList.js
@@ -0,0 +1,23 @@
+export const popularTabs = [
+  {
+    id: 'On_TV',
+    title: 'On TV',
+  },
+
+  {
+    id: 'In_Theaters',
+    title: 'In Theaters',
+  },
+];
+
+export const trendingTabs = [
+  {
+    id: 'Today',
+    title: 'Today',
+  },
+
+  {
+    id: 'This_Week',
+    title: 'This Week',
+  },
+];
