4dbcfb3c09221dd4ee823cddb9bb9f89563312c4
Vishal Rathod <rvd31305@gmail.com>
2022-03-23 17:36:03 +0530

    Removed unused constants

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,9 +1,14 @@
 export const API = process.env.REACT_APP_API_KEY;
-export const baseUrl = `https://api.themoviedb.org/3`;
-export const fetchPopularMovies = `${baseUrl}/movie/popular?api_key=${API}&language=en-US&page=1`;
-export const fetchNowPlaying = `${baseUrl}/movie/now_playing?api_key=${API}&language=en-US&page=1`;
-export const fetchPopular = `${baseUrl}/movie/popular?api_key=${API}&language=en-US&page=1`;
-export const fetchUpcoming = `${baseUrl}/movie/upcoming?api_key=${API}&language=en-US&page=1`;
+
+export const API_URL = 'https://api.themoviedb.org/3';
+
+export const SEARCH_URL = `${API_URL}/search/multi?api_key=${API}&query=`;
+
+export const POPULAR_ON_TV = `${API_URL}/tv/popular?api_key=${API}`;
+export const POPULAR_IN_THEATERS = `${API_URL}/movie/now_playing?api_key=${API}`;
+
+export const TRENDING_TODAY = `${API_URL}/trending/all/day?api_key=${API}`;
+export const TRENDING_THIS_WEEK = `${API_URL}/trending/all/week?api_key=${API}`;
 
 const BASE_URL = 'https://www.themoviedb.org/t/p';
 export const LOGO_URL =
@@ -14,15 +19,3 @@ export const BACKDROP_URL = `${BASE_URL}/w1920_and_h800_multi_faces`;
 export const STREAMING_URL = `${BASE_URL}/original`;
 export const CAST_URL = `${BASE_URL}/w138_and_h175_face`;
 export const NETWORK_URL = `${BASE_URL}/h30`;
-
-export const API_URL = 'https://api.themoviedb.org/3';
-export const TRENDING_DAY = `${API_URL}/trending/all/day?api_key=${API}`;
-export const TRENDING_WEEK = `${API_URL}/trending/all/week?api_key=${API}`;
-
-export const SEARCH_URL = `${API_URL}/search/multi?api_key=${API}&query=`;
-
-export const POPULAR_ON_TV = `${API_URL}/tv/popular?api_key=${API}`;
-export const POPULAR_IN_THEATERS = `${API_URL}/movie/now_playing?api_key=${API}`;
-
-export const TRENDING_TODAY = `${API_URL}/trending/all/day?api_key=${API}`;
-export const TRENDING_THIS_WEEK = `${API_URL}/trending/all/week?api_key=${API}`;
