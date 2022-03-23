c3906d88944a39c679662c6974cb71edbbdfdee2
Vishal Rathod <rvd31305@gmail.com>
2022-03-23 17:28:41 +0530

    Added routing to movie details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
+import MovieDetail from './components/MovieDetail/MovieDetail';
 import Footer from './components/Footer/Footer';
 
 import './app.scss';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/movie/:id' element={<MovieDetail />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 };
 
