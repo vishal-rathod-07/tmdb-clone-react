9fe196c946992371f7769ddd594b86d6cd1fb993
Vishal Rathod <rvd31305@gmail.com>
2022-03-23 17:27:51 +0530

    Refactor: Logo Url
    - Logo replaced with constant logo url variable

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import './navbar.scss';
+import { LOGO_URL } from '../Constants';
 
 import { useEffect, useState } from 'react';
 
@@ -35,7 +36,7 @@ const Navbar = () => {
         <div className='container-fluid navbar-inner'>
           <a className='navbar-brand' href='#'>
             <img
-              src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
+              src={LOGO_URL}
               alt='The Movie Database (TMDB)'
               width='154'
               height='20'
