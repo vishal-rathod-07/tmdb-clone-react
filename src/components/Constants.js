8a023b66594e64301ffb7d00125c02a43a9f81f6
Vishal Rathod <rvd31305@gmail.com>
2022-03-29 18:47:27 +0530

    Added path for categories

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -10,6 +10,13 @@ export const POPULAR_IN_THEATERS = `${API_URL}/movie/now_playing?api_key=${API}`
 export const TRENDING_TODAY = `${API_URL}/trending/all/day?api_key=${API}`;
 export const TRENDING_THIS_WEEK = `${API_URL}/trending/all/week?api_key=${API}`;
 
+// Categories
+
+export const NOW_PLAYING = `${API_URL}/movie/now_playing?api_key=${API}`;
+export const UPCOMING = `${API_URL}/movie/upcoming?api_key=${API}`;
+export const TOP_RATED = `${API_URL}/movie/top_rated?api_key=${API}`;
+export const POPULAR = `${API_URL}/movie/popular?api_key=${API}`;
+
 const BASE_URL = 'https://www.themoviedb.org/t/p';
 export const LOGO_URL =
   'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg';
