0efdcbc387faf7e76ecbb81f4f85773ffe7cc3d8
Vishal Rathod <rvd31305@gmail.com>
2022-03-30 15:46:33 +0530

    Refactor: sorting added, TV category added

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,15 +3,15 @@ import './movies.scss';
 
 import { useParams } from 'react-router-dom';
 
-import { NOW_PLAYING, UPCOMING, TOP_RATED, POPULAR } from '../Constants';
-
 import FilterMovieCard from '../FilterMovieCard/FilterMovieCard';
 import { Dropdown, Accordion, DropdownButton } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Movies = () => {
-  const { type } = useParams();
+  const { showType, type } = useParams();
+
+  // console.log(showType);
 
   useEffect(() => {
     setPage(1);
@@ -27,7 +27,9 @@ const Movies = () => {
 
   const [totalPages, setTotalPages] = useState(null);
 
-  console.log(page);
+  const [sortBy, setSortBy] = useState(null);
+
+  // console.log(page);
 
   useEffect(() => {
     setLoading(true);
@@ -35,15 +37,18 @@ const Movies = () => {
     setMovies(null);
     setHasMore(false);
     setTotalPages(null);
+    setSortBy(null);
 
     fetch(
-      `https://api.themoviedb.org/3/movie/${type}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/${
+        showType === 'tv' ? 'tv/' : 'movie/'
+      }${type}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
     )
       .then((response) => response.json())
       .then((data) => {
         setMovies(data.results);
-        console.log(data.total_pages);
-        console.log('====', data.page);
+        // console.log(data.total_pages);
+        // console.log('====', data.page);
         setTotalPages(data.total_pages);
         setLoading(false);
       })
@@ -51,14 +56,14 @@ const Movies = () => {
         setError(error);
         setLoading(false);
       });
-  }, [type]);
+  }, [showType, type]);
 
   const [filteredMovies, setFilteredMovies] = useState(movies);
   const [filtersArray, setFiltersArray] = useState([]);
 
-  // console.log(filteredMovies);
+  console.log(filteredMovies);
 
-  movies && console.log(movies.length);
+  // movies && console.log(movies.length);
 
   useEffect(() => {
     setFilteredMovies(movies);
@@ -70,9 +75,11 @@ const Movies = () => {
     setPage(page + 1);
 
     fetch(
-      `https://api.themoviedb.org/3/movie/${type}?api_key=${
-        process.env.REACT_APP_API_KEY
-      }&language=en-US&page=${page + 1}`
+      `https://api.themoviedb.org/3/${
+        showType === 'tv' ? 'tv/' : 'movie/'
+      }${type}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${
+        page + 1
+      }`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -95,11 +102,13 @@ const Movies = () => {
     const fetchFilter = async () => {
       try {
         const filterResponse = await fetch(
-          `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+          `https://api.themoviedb.org/3/genre/${
+            showType === 'tv' ? 'tv/' : 'movie/'
+          }list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
         );
         const data = await filterResponse.json();
         setFilterList(data.genres);
-        console.log('Filter Fetch Sucess');
+        // console.log('Filter Fetch Sucess');
       } catch (error) {
         setError(error);
       }
@@ -107,18 +116,107 @@ const Movies = () => {
     fetchFilter();
   }, []);
 
-  //Fetch filter
-
-  // console.log(filterList);
-  // console.log(movies);
-
   const onMovieClick = (movie) => {
     console.log(movie);
   };
 
+  const handleSearchClick = (e) => {
+    if (sortBy) {
+      switch (sortBy) {
+        case '1':
+          setFilteredMovies(movies.sort((a, b) => b.popularity - a.popularity));
+          break;
+        case '2':
+          setFilteredMovies(movies.sort((a, b) => a.popularity - b.popularity));
+          break;
+        case '3':
+          setFilteredMovies(
+            movies.sort((a, b) => b.vote_average - a.vote_average)
+          );
+          break;
+        case '4':
+          setFilteredMovies(
+            movies.sort((a, b) => a.vote_average - b.vote_average)
+          );
+          break;
+        case '5':
+          setFilteredMovies(
+            movies.sort(
+              showType === 'tv'
+                ? movies.sort((a, b) => b.first_air_date - a.first_air_date)
+                : movies.sort((a, b) => b.release_date - a.release_date)
+            )
+          );
+          break;
+        case '6':
+          setFilteredMovies(
+            movies.sort(
+              showType === 'tv'
+                ? movies.sort(
+                    (a, b) =>
+                      new Date(a.first_air_date) - new Date(b.first_air_date)
+                  )
+                : movies.sort(
+                    (a, b) =>
+                      new Date(a.release_date) - new Date(b.release_date)
+                  )
+            )
+          );
+          break;
+
+        case '7':
+          setFilteredMovies(
+            showType === 'tv'
+              ? movies.sort((a, b) => b.name.localeCompare(a.name)) //sort by title
+              : movies.sort((a, b) => b.title.localeCompare(a.title))
+          );
+          break;
+        case '8':
+          setFilteredMovies(
+            showType === 'tv'
+              ? movies.sort((a, b) => a.name.localeCompare(b.name)) //sort by title
+              : movies.sort((a, b) => a.title.localeCompare(b.title)) //sort by title
+          );
+          break;
+        default:
+          setFilteredMovies(movies);
+          break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (filtersArray.length > 0) {
+      const filterMovies = movies.filter((movie) => {
+        const movieGenres = movie.genre_ids;
+        let isIncluded = false;
+        movieGenres.forEach((genre) => {
+          if (filtersArray.includes(genre)) {
+            isIncluded = true;
+          }
+        });
+        return isIncluded;
+      });
+      setFilteredMovies(filterMovies);
+    } else {
+      setFilteredMovies(movies);
+    }
+  }, [movies, filtersArray]);
+
+  // useEffect(() => {
+  //   if (sortBy) {
+  //     const filtered = movies.filter((movie) => {
+  //       return movie.genre_ids.includes(sortBy);
+  //     });
+  //     setFilteredMovies(filtered);
+  //   } else {
+  //     setFilteredMovies(movies);
+  //   }
+  // }, [sortBy]);
+
   // console.log(filtersArray);
 
-  console.log(hasMore);
+  // console.log(hasMore);
 
   const toggleFilter = (filter) => {
     if (filtersArray.includes(filter)) {
@@ -137,8 +235,6 @@ const Movies = () => {
       return movies;
     }
   };
-
-  const activeTab = 'In_Theaters';
 
   return (
     <section className='content container'>
@@ -183,6 +279,10 @@ const Movies = () => {
                             fontWeight: '300',
                             marginBottom: '10px',
                           }}
+                          onSelect={(eventKey) => {
+                            console.log(eventKey);
+                            setSortBy(eventKey);
+                          }}
                         >
                           <Dropdown.Item eventKey='1'>
                             Popularity Descending
@@ -196,16 +296,16 @@ const Movies = () => {
                           <Dropdown.Item eventKey='4'>
                             Rating Ascending
                           </Dropdown.Item>
-                          <Dropdown.Item eventKey='4'>
+                          <Dropdown.Item eventKey='5'>
                             Release Date Descending
                           </Dropdown.Item>
-                          <Dropdown.Item eventKey='4'>
+                          <Dropdown.Item eventKey='6'>
                             Release Date Ascending
                           </Dropdown.Item>
-                          <Dropdown.Item eventKey='4'>
+                          <Dropdown.Item eventKey='7'>
                             Title (A-Z)
                           </Dropdown.Item>
-                          <Dropdown.Item eventKey='4'>
+                          <Dropdown.Item eventKey='8'>
                             Title (Z-A)
                           </Dropdown.Item>
                         </DropdownButton>
@@ -284,12 +384,22 @@ const Movies = () => {
                     </Accordion.Item>
                   </Accordion>
                 </div>
-                <div className='search-btn'>Search</div>
+                <div className='search-btn' onClick={handleSearchClick}>
+                  Search
+                </div>
               </div>
               <div className='movies-section'>
                 <div className='wrapper'>
                   <section className='pannel'>
                     <div className='media_items'>
+                      {loading ? (
+                        <div className='loader'>
+                          <img
+                            src={require('../../assets/images/loader.gif')}
+                            alt='loader'
+                          />
+                        </div>
+                      ) : null}
                       {movies && (
                         <InfiniteScroll
                           dataLength={movies.length}
@@ -302,15 +412,13 @@ const Movies = () => {
                               <img
                                 src={require('../../assets/images/loader.gif')}
                                 alt='loader'
-                                height={'10px'}
-                                width={'10px'}
                               />
                             </div>
                           }
                         >
                           <div className='page_1'>
-                            {movies &&
-                              movies.map((movie, index) => (
+                            {filteredMovies &&
+                              filteredMovies.map((movie, index) => (
                                 <FilterMovieCard
                                   key={index}
                                   id={movie.id}
@@ -321,7 +429,7 @@ const Movies = () => {
                                   }
                                   rating={movie.vote_average * 10}
                                   onMovieClick={onMovieClick}
-                                  activeTab={activeTab}
+                                  showType={showType}
                                   movie={movie}
                                 />
                               ))}
